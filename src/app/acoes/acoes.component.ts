d7e217990ba638be6809697e68e868d735de2807
Wellysson Rocha <33838737+Werisu@users.noreply.github.com>
2022-10-03 14:48:48 -0300

    Filter, DebounceTime, DistinctUntilChanged

--- src/app/acoes/acoes.component.ts
+++ src/app/acoes/acoes.component.ts
@@ -2,8 +2,9 @@ import { Subscription, merge } from 'rxjs';
 import { AcoesService } from './acoes.service';
 import { Component, OnInit, OnDestroy } from '@angular/core';
 import { FormControl } from '@angular/forms';
-import { Acoes } from './modelo/acoes';
-import { tap, switchMap } from 'rxjs/operators';
+import { tap, switchMap, filter, debounceTime, distinctUntilChanged } from 'rxjs/operators';
+
+const ESPERA_DIGITACAO = 300;
 
 @Component({
   selector: 'app-acoes',
@@ -18,10 +19,15 @@ export class AcoesComponent implements OnInit, OnDestroy {
     })
   );
   public filtroPeloInput$ = this.acoesInput.valueChanges.pipe(
+    debounceTime(ESPERA_DIGITACAO),
     tap(() => {
       console.log('Fluxo do filtro');
     }),
-    switchMap((valorDigitado) => this.acoesService.getAcoes(valorDigitado))
+    tap(console.log),
+    filter((valorDigitado) => valorDigitado.length >= 3 || !valorDigitado.length),
+    distinctUntilChanged(),
+    switchMap((valorDigitado) => this.acoesService.getAcoes(valorDigitado)),
+    tap(console.log)
   );
 
   public acoes$ = merge(this.todasAcoes$, this.filtroPeloInput$);
